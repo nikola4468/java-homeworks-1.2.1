d193b41e45644595706ca5cdd401436260b14611
nikola4468 <nikola4468@gmail.com>
2021-09-01 15:24:03 +0900

    initial commit

---
+++ src/MoneyTransfer.java
@@ -0,0 +1,8 @@
+public class MoneyTransfer {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int remittance = 500_000_000;
+        int balance_new = balance + remittance;
+        System.out.println(balance_new);
+    }
+}
